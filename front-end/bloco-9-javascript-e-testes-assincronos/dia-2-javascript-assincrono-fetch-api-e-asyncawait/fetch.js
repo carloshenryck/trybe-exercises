c2c7df02ed6044377674b15c14c0c61b2d346b61
Henryck <ryck302@gmail.com>
2022-05-25 00:49:25 -0300

    ajeitar fetch.js

--- front-end/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/fetch.js
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/fetch.js
@@ -1,14 +1,12 @@
 const fetch = require('node-fetch');
 
-const fetchJoke = async () => {
+const fetchJoke = () => {
   const url = 'https://api.chucknorris.io/jokes/random?category=dev';
 
-  const result = await fetch(url)
+  const result = fetch(url)
     .then((response) => response.json())
-    .then((data) => console.log(data.value))
+    .then((data) => console.log(data.value)) 
     .catch((error) => console.log(`Algo deu errado :( \n${error}`));
-
-  console.log(result);  
 }
 
 fetchJoke();
