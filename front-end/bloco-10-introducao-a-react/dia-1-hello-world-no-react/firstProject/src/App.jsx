ebec421d9c819b0ad79d0ecf3b79a0ab16e05025
Henryck <ryck302@gmail.com>
2022-05-27 17:36:00 -0300

    erro no router

--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/firstProject/src/App.jsx
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/firstProject/src/App.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import {v4 as uuidv4} from 'uuid'
+import { BrowserRouter as Router, Route} from "react-router-dom";
 
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
+import TaskDetails from './components/TaskDetails';
+
 import './App.css'
 
 const App = () => {
@@ -47,18 +50,28 @@ const App = () => {
   }
 
   return (
-    <>
+    <Router>
       <div className="container">
         <Header />
-        <AddTask handleTaskAddition={handleTaskAddition} />
-        <Tasks 
-          tasks={tasks} 
-          handleTaskClick={handleTaskClick} 
-          handleTaskDeletion={handleTaskDeletion}
+        <Route 
+          path='/' 
+          exact 
+          render={() => (
+            <>
+              <AddTask handleTaskAddition={handleTaskAddition} />
+              <Tasks 
+                tasks={tasks} 
+                handleTaskClick={handleTaskClick} 
+                handleTaskDeletion={handleTaskDeletion}
+              />
+            </> 
+          )} 
         />
+        
+        <Route path='/:taskTitle' exact component={TaskDetails}/>
       </div>
-    </>
-  )
-}
+    </Router>
+  );
+};
 
 export default App;
--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/firstProject/src/components/Tasks.jsx
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/firstProject/src/components/Tasks.jsx
@@ -4,7 +4,13 @@ import Task from './Task';
 const Tasks = ({ tasks, handleTaskClick, handleTaskDeletion }) => {
   return (
     <>
-      {tasks.map(task => <Task task={task} handleTaskClick={handleTaskClick} handleTaskDeletion={handleTaskDeletion} />)}
+      {tasks.map(task => (
+        <Task 
+          task={task} 
+          handleTaskClick={handleTaskClick} 
+          handleTaskDeletion={handleTaskDeletion} 
+        />
+      ))}
     </>
   );
 }
